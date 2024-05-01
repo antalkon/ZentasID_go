4e251555b94c388db07e1303b2044394ed53da57
Antipev Aleksei <87186332+antalkon@users.noreply.github.com>
2024-05-01 23:40:42 +0500

    Redir and rename

--- router/authRouter.go
+++ router/authRouter.go
@@ -1,17 +1,17 @@
 package router
 
 import (
-	"TestWebServer/controllers"
+	"TestWebServer/controllers/auth"
 	"github.com/gorilla/mux"
 )
 
 // SetAuthRoutes устанавливает маршруты для аутентификации и регистрации
 func SetAuthRoutes(router *mux.Router) {
-	router.HandleFunc("/api/auth/reg", controllers.RegisterHandler).Methods("POST")
-	router.HandleFunc("/api/auth/login/step1", controllers.LoginHandler).Methods("POST")
-	router.HandleFunc("/api/auth/refresh", controllers.RefreshTokenHandler).Methods("GET")
-	router.HandleFunc("/api/auth/logout", controllers.LogoutHandler).Methods("GET")
-	router.HandleFunc("/api/auth/login/step2", controllers.Login2Handler).Methods("POST")
-	router.HandleFunc("/api/auth/verify/{token}", controllers.VerifyHandler).Methods("GET")
+	router.HandleFunc("/api/auth/reg", auth.RegisterHandler).Methods("POST")
+	router.HandleFunc("/api/auth/login/step1", auth.LoginHandler).Methods("POST")
+	router.HandleFunc("/api/auth/refresh", auth.RefreshTokenHandler).Methods("GET")
+	router.HandleFunc("/api/auth/logout", auth.LogoutHandler).Methods("GET")
+	router.HandleFunc("/api/auth/login/step2", auth.Login2Handler).Methods("POST")
+	router.HandleFunc("/api/auth/verify/{token}", auth.VerifyHandler).Methods("GET")
 
 }
