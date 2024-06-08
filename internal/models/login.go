5982099e2964061dab44f0bd1f8a40263933f39b
Antipev Aleksei <87186332+antalkon@users.noreply.github.com>
2024-06-08 13:33:15 +0500

    Create temp code login

--- internal/models/login.go
+++ internal/models/login.go
@@ -8,3 +8,8 @@ type UserCheckPhone struct {
 	UserID string `json:"userid"`
 	Verify bool   `json:"verify"`
 }
+
+type UserCheckEmail struct {
+	UserID string `json:"userid"`
+	Verify bool   `json:"verify"`
+}
