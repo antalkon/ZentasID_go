209e4f24c69560d49c756c3d42c05118dc44158d
Antipev Aleksei <87186332+antalkon@users.noreply.github.com>
2024-07-01 09:34:26 +0500

    Redirecto post login (no prod)

--- internal/services/auth/login/sLoginStep2.go
+++ internal/services/auth/login/sLoginStep2.go
@@ -53,6 +53,7 @@ func StdLoginStep2Api(c *gin.Context) {
 		c.SetCookie("refresh_token", refresh, 31*24*60*60, "/", "localhost:8080", false, true)
 
 		c.JSON(http.StatusOK, gin.H{"success": "Вход выполнен!", "checked": checked})
+		c.Redirect(http.StatusMovedPermanently, "https://id.zentas.ru/")
 		return
 	}
 
@@ -91,6 +92,8 @@ func StdLoginStep2Api(c *gin.Context) {
 		c.SetCookie("refresh_token", refresh, 31*24*60*60, "/", "localhost:8080", false, true)
 
 		c.JSON(http.StatusOK, gin.H{"success": "Вход выполнен!", "checked": checked})
+		c.Redirect(http.StatusMovedPermanently, "https://id.zentas.ru/")
+
 		return
 	}
 
